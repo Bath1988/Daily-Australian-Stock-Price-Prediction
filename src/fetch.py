8ca76c3aedd0590fd9605f7d6ab434cff33547a4
Bhathiya <bathiya.rg@gmail.com>
2025-05-10 21:03:10 +1000

    fetch.py fetch data from the API. Previously this code was in app.py. Now app.py includes the model and the endpoint.

---
+++ src/fetch.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history"
+
+querystring = {"symbol":"CBA.AX","interval":"1d","diffandsplits":"false"}
+
+headers = {
+	"x-rapidapi-key": "d79a3883b7msh00d863286e8567bp132781jsn1518cc26254e",
+	"x-rapidapi-host": "yahoo-finance15.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Convert the response to JSON format
+    data = response.json()
+
+    # Pretty print the JSON data
+    print(json.dumps(data, indent=4))  # Using json.dumps to format the output
+    
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+else:
+    print(f"Error: Unable to fetch data. Status code {response.status_code}")
+
+
+    
+
+
+
